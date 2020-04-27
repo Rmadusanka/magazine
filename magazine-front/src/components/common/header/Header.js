a2ad647e740dd0e41d388caf88681e1d4135b2ef
roshan <roshanmw91@gmail.com>
2020-04-27 11:48:48 +0530

    Header styles implement

--- magazine-front/src/components/common/header/Header.js
+++ magazine-front/src/components/common/header/Header.js
@@ -1,13 +1,38 @@
 import React, { Component } from 'react'
 
+// styles
 import './Header.scss';
+
+// images 
+import logo from '../../../assets/images/color-logo-dark-background.svg';
+import font from '../../../assets/images/icons/font-size.svg';
+import share from '../../../assets/images/icons/share.svg';
+import menu from '../../../assets/images/icons/menu.svg';
+
 
 export default class Header extends Component {
     render() {
         return (
             <div className="mn-header">
-                <div>
-                    Header
+                <div className="header-left-content">
+                    <img className="left-header-logo" src={logo} />
+                    <div className="magazine-date">May 2020</div>
+                </div>
+                <div className="header-center-content">
+                    <img className="header-logo" src={logo} />
+                </div>
+                <div className="header-right-content">
+                    <div className="header-actions">
+                        <button>
+                            <img src={font} />
+                        </button>
+                        <button>
+                            <img src={share} />
+                        </button>
+                        <button id="headerMenuBtn" className="header-menu-btn">
+                            <img src={menu} />
+                        </button>
+                    </div>
                 </div>
             </div>
         )
