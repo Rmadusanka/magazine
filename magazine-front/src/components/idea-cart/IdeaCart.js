33fecedf9f01e03869261d6b992a88d917af02aa
AmithLiyanage <amith.liyanage.9@gmail.com>
2020-05-06 16:58:25 +0530

    set color styles and added idea cart logo

--- magazine-front/src/components/idea-cart/IdeaCart.js
+++ magazine-front/src/components/idea-cart/IdeaCart.js
@@ -1,15 +1,21 @@
 import React, { Component } from 'react'
 
+//CSS Styles
 import './IdeaCart.scss';
+
+// Images
+import ideaCartLogo from '../../assets/images/ideacart/idea-cart-logo.svg';
 
 export default class IdeaCart extends Component {
     render() {
         return (
             <div className="idea-cart-page">
                 <div className="top-spacer"></div>
-                <div className="page-container">
-                    <div className="page-title">{this.props.pageTitle}</div>
-
+                <div className="idea-cart-container">
+                    <div className="idea-cart-title">{this.props.pageTitle}</div>
+                    <div className="idea-cart-logo">
+                        <img src={ideaCartLogo} />
+                    </div>
                 </div>
             </div>
         )
